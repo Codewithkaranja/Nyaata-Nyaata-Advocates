16abe0c8d046fd479803887d6eae650041e28c9f
Codewithkaranja <codewithkaranja@gmail.com>
2025-11-27 08:13:01 +0300

    Implemented 3D Swiper carousel for Services section

--- index.js
+++ index.js
@@ -82,25 +82,4 @@ document.addEventListener("DOMContentLoaded", function () {
   typeEffect();
 });
 
- const servicesSwiper = new Swiper(".services-swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    loop: true,
-    coverflowEffect: {
-      rotate: 30,
-      stretch: 0,
-      depth: 200,
-      modifier: 1,
-      slideShadows: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  });
+
